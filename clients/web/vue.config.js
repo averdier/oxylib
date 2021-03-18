b06138190bed7ad02d3eeb06e14d855f9907b305
RastaDev <a.verdier@outlook.fr>
2021-03-18 13:50:42 +0100

    Link service worker and add README.md

--- clients/web/vue.config.js
+++ clients/web/vue.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  pwa: {
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      swSrc: 'src/sw.js',
+      swDest: 'service-worker.js',
+      importWorkboxFrom: 'local'
+    }
+  }
 }
